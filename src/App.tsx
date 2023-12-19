5d8bfce0bdf3f90aa873842035b34d394124e8dc
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2023-12-19 23:26:07 +0200

    .

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {chartData} from './data';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, HiloOpenCloseSeries, DateTime,
+  Inject, Zoom, ScrollBar} from '@syncfusion/ej2-react-charts';
+
+function App() {
+  return (
+    <div style={{textAlign:"center", margin:'5%'}}>
+      <ChartComponent 
+      primaryXAxis={{valueType:"DateTime", title: 'Month', skeleton: 'yMMM',
+      edgeLabelPlacement:"Shift", enableAutoIntervalOnZooming:true}} primaryYAxis={{title:"Price"}}
+      zoomSettings={{enableSelectionZooming:true, enablePan:true, enableScrollbar:true, mode:"X"}}>
+        <Inject services={[HiloOpenCloseSeries, DateTime, Zoom, ScrollBar]}></Inject>
+        <SeriesCollectionDirective>
+          <SeriesDirective type="HiloOpenClose" dataSource={chartData}
+          xName="date" high="high" low="low" open="open" close="close"></SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+}
+export default App;
+
